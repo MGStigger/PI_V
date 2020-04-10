4e0aa4f357d502b5bc446b04e018a12982340aa2
Matheus Gonçalves Stigger <54038393+MGStigger@users.noreply.github.com>
2020-04-10 20:37:17 -0300

    Envio de conteúdos - Broker MQTT

--- Atividade - 27.03.2020/mqtt2.py
+++ Atividade - 27.03.2020/mqtt2.py
@@ -1,4 +1,7 @@
-import context  # Ensures paho is in PYTHONPATH
+# Ensures paho is in PYTHONPATH
+import context
+# Importa o publish do paho-mqtt
 import paho.mqtt.publish as publish
 
+# Publica
 publish.single("mgstigger", "Olá Mundo!", hostname="mqtt.eclipse.org")
