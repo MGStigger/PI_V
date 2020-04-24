248349c5d615639ff80808b5bda456371515b312
MGStigger <stigger1999@gmail.com>
2020-04-24 19:02:05 -0300

    Update Arduino-Python-ThingSpeak

---
+++ Arduino-Python-ThingSpeak/serial-thingspeak-publish.py
@@ -0,0 +1,46 @@
+# Channel: https://thingspeak.com/channels/1042952 .
+
+# Importa o publish da biblioteca paho.mqtt
+import paho.mqtt.publish as publish
+
+#Importa a biblioteca serial
+import serial
+
+# Importa a biblioteca time
+import time
+
+# Variavel "host_name" recebe o broker do ThingSpeak
+host_name = "mqtt.thingspeak.com"
+
+# Numero do campo (field) que se deseja enviar o valor da temperatura
+numero_campo = 1
+
+# ID do Canal
+channel_id = 1042952
+
+# Chave para gravar dados no canal
+write_key = 'HHIO70MX4XESGDBJ'
+
+# Topico
+topico = "channels/"+str(channel_id)+"/publish/fields/field"+(numero_campo)+"/"+write_key
+
+# Variavel temperatura
+temperatura = 0
+
+# Variavel "ser" recebe a porta serial
+ser = serial.Serial('/dev/ttyACM0')
+
+# Taxa de transmissao
+ser.baudrate = 9600
+
+#ser.port = '/dev/ttyACM0'
+
+# Abre a porta serial
+ser.open()
+
+while 'true':
+	# Le uma linha terminada com "\n"
+	temperatura = ser.readline()
+    	#print(temperatura)
+	# Envia uma mensagem do cliente para o broker
+	publish.single(topico, temperatura, hostname=host_name)
--- Arduino-Python-ThingSpeak/serial-thingspeak.py
+++ Arduino-Python-ThingSpeak/serial-thingspeak.py
@@ -1,19 +1,43 @@
 # Channel: https://thingspeak.com/channels/1042952 .
 
+#IMPORTA AS BIBLIOTECAS
 import serial
 import time
 import thingspeak
-channel_id = channel_id
-write_key = 'write_key'
+
+#ID DO CANAL
+channel_id = 1042952
+
+#CHAVE PARA GRAVAR DADOS NO CANAL
+write_key = 'HHIO70MX4XESGDBJ'
+
+#INICIALIZA A VARIAVEL "temperatura" COM O VALOR ZERO
 temperatura = 0
-ser = serial.Serial()
+
+#VARIAVEL "ser" RECEBE A PORTA SERIAL
+ser = serial.Serial('/dev/ttyACM0')
+
+#TAXA DE TRANSMISSAO
 ser.baudrate = 9600
-ser.port = '/dev/ttyACM0'
+
+#ser.port = '/dev/ttyACM0'
+
+#ABRE A PORTA SERIAL
 ser.open()
+
+#CANAL
 channel = thingspeak.Channel(id=channel_id, write_key=write_key)
+
 while 'true':
+	#VARIAVEL "temperatura" FAZ A LEITURA DE UMA LINHA TERMINADA COM "\n"
 	temperatura = ser.readline();
-	time.sleep(5)
+
+	#time.sleep(5)
+
+	#IMPRIME O VALOR DA TEMPERATURA
     	print(temperatura)
+
+	#ENVIA O DADO DA TEMPERATURA PARA O CANAL DO THINGSPEAK
     	response = channel.update({1:temperatura})
-    	time.sleep(5)
+
+    	#time.sleep(5)
